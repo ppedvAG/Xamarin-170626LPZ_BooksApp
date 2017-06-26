085e6e6695b424d05f80da304646d41952812c78
Michael Zöhling <michael.zoehling@outlook.com>
2017-06-26 15:54:57 +0200

    BooksApp V1.0

--- Presentation/BooksAppAndroid/MainActivity.cs
+++ Presentation/BooksAppAndroid/MainActivity.cs
@@ -1,18 +1,43 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using BooksApp.Services.DataServices;
+using BooksApp.Contracts.Models;
+using System.Linq;
 
 namespace BooksAppAndroid
 {
     [Activity(Label = "BooksAppAndroid", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private Button buttonSearch;
+        private EditText editTextSearchtext;
+        private ListView listViewBooks;
+
+        private BookService service;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            buttonSearch = FindViewById<Button>(Resource.Id.buttonSearch);       
+            editTextSearchtext = FindViewById<EditText>(Resource.Id.editTextSearchtext);       
+            listViewBooks = FindViewById<ListView>(Resource.Id.listViewBooks);
+
+            //Kontrollfreak-Antipattern
+            service = new BookService();
+
+            buttonSearch.Click += ButtonSearch_Click;
+        }
+
+        private async void ButtonSearch_Click(object sender, System.EventArgs e)
+        {
+            BookQuery result = await service.GetBooksAsync(editTextSearchtext.Text);
+
+            string[] data = result.Books.Select(x => x.Info.Title).ToArray();
+
+            listViewBooks.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, data);
         }
     }
 }
