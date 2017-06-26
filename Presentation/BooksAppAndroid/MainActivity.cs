2520cdba663cdc6a408c365de74c0e161b02a080
Michael Zöhling <michael.zoehling@outlook.com>
2017-06-26 15:25:58 +0200

    Android UI

---
+++ Presentation/BooksAppAndroid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BooksAppAndroid
+{
+    [Activity(Label = "BooksAppAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
